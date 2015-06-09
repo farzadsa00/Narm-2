9f47cf879749f838474ef17117b722d088314b12
farzadsa00 <farzad.kh72@gmail.com>
2015-06-09 16:13:25 +0430

    test1

---
+++ BLL/StudentInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BLL
+{
+    class StudentInfo
+    {
+        public string StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Id { get; set; }
+        public int EntryYear { get; set; }
+        public bool DormitoryStatus { get; set; }
+        public bool CardStatus { get; set; }
+        public string Password { get; set; }
+    }
+}
