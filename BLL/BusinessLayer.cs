8ebd8863b06575013f01ec9b349841b57b39bcc2
farzad <farzad.kh72@gmail.com>
2015-06-14 00:04:18 +0430

    farzad

--- BLL/BusinessLayer.cs
+++ BLL/BusinessLayer.cs
@@ -9,16 +9,42 @@ namespace BLL
 {
     public class BusinessLayer
     {
-        //use case
+        public bool ActivateCard(StudentInfo student)
+        {
+            throw new NotImplementedException();
+        }
         public string ReserveFood(StudentInfo student, FoodInfo food,
             FoodScheduleInfo foodSchedule, ResturantInfo resturant)
+        {
+            string result = "";
+            if (!student.CardStatus)
+                return "Card not activated.";
+            using (TransactionScope ts = new TransactionScope())
+            {
+                throw new NotImplementedException();
+            }
+            return result;
+        }
+        public string Charge(StudentInfo student, double amount)
+        {
+            string result = "";
+            using (TransactionScope ts=new TransactionScope())
+            {
+                throw new NotImplementedException();
+            }
+            return result;
+        }
+        public bool RequestBalance(StudentInfo student)
         {
             using (TransactionScope ts = new TransactionScope())
             {
                 throw new NotImplementedException();
             }
         }
-
+        public string ChangePassword(StudentInfo student,string newPassword)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
